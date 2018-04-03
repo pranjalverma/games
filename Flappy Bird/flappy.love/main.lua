88cdc2876bf1e2e2d7b410bdbd75ca20c47a47c7
Pranjal Verma <pranjalverma.wiki@gmail.com>
2018-04-04 00:01:49 +0530

    Add files via upload
    parallax and gravity update

--- Flappy Bird/flappy.love/main.lua
+++ Flappy Bird/flappy.love/main.lua
@@ -5,8 +5,15 @@ AUTHOR: PRANJAL VERMA
 
 ]]--
 
--- Game Dimensions
+-- USE LÖVELYMOON
+
+-- requires
+class = require 'middleclass'
+require 'Bird'
+
+-- Global game constants
 WINDOW_WIDTH, WINDOW_HEIGHT = 1440, 900
+GRAVITY = 20
 
 -- Game inits
 love.window.setTitle('Flappy Bird')
@@ -15,40 +22,81 @@ love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
 	highdpi = true -- only for Retina Displays
 	})
 
+-- Bird object
+local bird = Bird()
+
 -- Game backdrop object
 local backdrop = {
 	image = love.graphics.newImage('Images/backdrop.png'),
 	scrollAmount = 0,
 	SCROLL_SPEED = 30,
+	LOOPING_POINT = WINDOW_WIDTH
+}
+
+-- Game ground object
+local ground = {
+	image = love.graphics.newImage('Images/ground.png'),
+	scrollAmount = 0,
+	SCROLL_SPEED = 4 * backdrop.SCROLL_SPEED,
+	LOOPING_POINT = 100
 }
 
 -- Callback for game init
 function love.load()
 	love.graphics.setDefaultFilter('nearest', 'nearest')
+
+	-- creating table to track single key presses, frame by frame
+	love.keyboard.keysPressed = {} 
 end
 
 -- Callback for updating game state
 function love.update(dt)
 
-	-- backdrop scroll amount; fps independent
+	-- backdrop and ground scroll amount
 	backdrop.scrollAmount = (backdrop.scrollAmount + backdrop.SCROLL_SPEED * dt)
-							% WINDOW_WIDTH
+							% backdrop.LOOPING_POINT
+	ground.scrollAmount = (ground.scrollAmount + ground.SCROLL_SPEED * dt)
+							% ground.LOOPING_POINT
+
+	-- update bird
+	bird:update(dt)
+
+	-- flushing single-key-presses tracker at end of every frame
+	love.keyboard.keysPressed = {}
 
 end
 
 -- Callback for drawing updated game state on the screen
 function love.draw()
 
-	-- drawing backdrop with infinite scrolling
+	-- drawing backdrop and ground with infinite scrolling
 	love.graphics.draw(backdrop.image, -backdrop.scrollAmount, 0)
-	love.graphics.draw(backdrop.image, WINDOW_WIDTH - backdrop.scrollAmount, 0)
+	love.graphics.draw(backdrop.image,
+		backdrop.LOOPING_POINT - backdrop.scrollAmount, 0)
+
+	for i=0,144 do
+		love.graphics.draw(ground.image,
+			i*100 - ground.scrollAmount, WINDOW_HEIGHT - 50)
+	end
+
+	-- draw bird
+	bird:render()
+
 end
 
--- Callback for game state controls
+-- Callback for game state and player controls
 function love.keypressed(key)
+
+	-- updating keysPressed tracker
+	love.keyboard.keysPressed[key] = true
 
 	-- game quit
 	if key == 'escape' or key == 'q' then
 		love.event.quit()
 	end
+end
+
+-- Query func for keysPressed to check if 'key' was pressed or not
+function love.keyboard.wasPressed(key)
+	return love.keyboard.keysPressed[key]
 end
